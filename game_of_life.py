91c022d5163aab49aefaa0b319b4811d23b6e0dd
arynh2 <arynh2@illinois.edu>
2018-01-27 23:55:37 -0600

    reformat to make it pretty?

--- game_of_life.py
+++ game_of_life.py
@@ -2,14 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import imageio
 
-#populate the field as a list of lists with type int zeroes
-(n,m) = (20,20)   #Define the size of the field
-field = np.zeros( (n,m), dtype=np.int )
+# populate the field as a list of lists with type int zeroes
+(n, m) = (20, 20)  # Define the size of the field
+field = np.zeros((n, m), dtype=np.int)
 
-#number of generations to run
+# number of generations to run
 generations = 65
 
-#assign initial condition
+# assign initial condition
 '''R-pentamino
 field[10,10] = 1
 field[9,10] = 1
@@ -19,19 +19,19 @@ field[10,9] = 1
 '''
 
 '''glider'''
-field[0,1] = 1
-field[1,2] = 1
-field[2,0] = 1
-field[2,1] = 1
-field[2,2] = 1
-
+field[0, 1] = 1
+field[1, 2] = 1
+field[2, 0] = 1
+field[2, 1] = 1
+field[2, 2] = 1
 
 plt.title('Initial Condition (generation = 0)')
-plt.imshow( field )
+plt.imshow(field)
 plt.show()
 plt.clf()
 
-def GOL( field ):
+
+def GOL(field):
     def get(l, p):
         try:
             return l[p]
@@ -44,9 +44,9 @@ def GOL( field ):
         except IndexError:
             return [0]
 
-    #define logic used to decide whether cell lives, dies, or is born
-    def output( inputMatrix ):
-        neighbors = inputMatrix[ 1: ]
+    # define logic used to decide whether cell lives, dies, or is born
+    def output(inputMatrix):
+        neighbors = inputMatrix[1:]
         if inputMatrix[0] == 0:
             if neighbors.count(1) == 3:
                 nextState = 1
@@ -69,43 +69,46 @@ def GOL( field ):
 
     currentCases = {}
 
-    #getCase() retrieves the states of the neighboring cells and puts them into a list to be read by output()
-    def getCase( i,j ):
+    # getCase() retrieves the states of the neighboring cells and puts them into a list to be read by output()
+    def getCase(i, j):
         currentCase = []
-        def addN( addition ):
-            currentCase.append( addition )
-        addN( get(getL(field,i),j) )
-        addN( get(getL(field,i-1),j-1) )
-        addN( get(getL(field,i-1),j) )
-        addN( get(getL(field,i-1),j+1) )
-        addN( get(getL(field,i),j-1) )
-        addN( get(getL(field,i),j+1) )
-        addN( get(getL(field,i+1),j-1) )
-        addN( get(getL(field,i+1),j+1) )
-        addN( get(getL(field,i+1),j) )
+
+        def addN(addition):
+            currentCase.append(addition)
+
+        addN(get(getL(field, i), j))
+        addN(get(getL(field, i - 1), j - 1))
+        addN(get(getL(field, i - 1), j))
+        addN(get(getL(field, i - 1), j + 1))
+        addN(get(getL(field, i), j - 1))
+        addN(get(getL(field, i), j + 1))
+        addN(get(getL(field, i + 1), j - 1))
+        addN(get(getL(field, i + 1), j + 1))
+        addN(get(getL(field, i + 1), j))
         return currentCase
 
-    for i in range( field.shape[0] ):
-        for j in range( field.shape[1] ):
-            currentCases[ (i,j) ] = getCase( i,j )
-    for (coordinate,case) in currentCases.items():
-        newfield[ coordinate[0],coordinate[1] ] = output( case )
+    for i in range(field.shape[0]):
+        for j in range(field.shape[1]):
+            currentCases[(i, j)] = getCase(i, j)
+    for (coordinate, case) in currentCases.items():
+        newfield[coordinate[0], coordinate[1]] = output(case)
 
     return newfield
+
 
 newf = field.copy()
 filenames = []
 
-for gen in range(1,generations+1):
-    newf = GOL( newf )
+for gen in range(1, generations + 1):
+    newf = GOL(newf)
     plt.title('(generation = %s)' % gen)
-    plt.imshow( newf )
-    #plt.show()
+    plt.imshow(newf)
+    # plt.show()
     plt.savefig('generation_%s.png' % gen)
     filenames.append('generation_%s.png' % gen)
     plt.clf()
-    
+
 images = []
 for filename in filenames:
     images.append(imageio.imread(filename))
-imageio.mimsave('gol.gif', images, duration = 0.01)
+imageio.mimsave('gol.gif', images, duration=0.01)
